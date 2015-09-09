a0ef104647817bf266d874139769895b1f2df24d
Alex Rohleder <alexrohleder96@outlook.com>
2015-09-08 23:33:51 -0300

    Fix charset problems

--- t5.hs
+++ t5.hs
@@ -63,17 +63,17 @@ _numera _ [] = []
 _numera i (x:xs) = (i, x) : _numera (i + 1) xs
 
 {-
-    a. [ (x,y) | x <- [1..5], even x, y <- [(x + 1)..6], odd y ]
+    a. [(x,y) | x <- [1..5], even x, y <- [(x + 1)..6], odd y]
         list comprehension, dois laços são feitos e dois filter são aplicados, a lista x recebe um
         filtro por números pares e a lista y por números impares, logo após isto um map é executado
         para construir uma tupla com x e y.
 
-    b. [ a ++ b | a <- ["lazy","big"], b <- ["frog", "dog"]]
+    b. [a ++ b| a <- ["lazy", "big"], b <- ["frog", "dog"]]
         mais uma função com list comprehension, esta expressão representa um duplo laço percorrendo
         cada elemento do a com cada elemento do b, sendo o b o laço interior ao laço do a. a
         concatenação é feita com a função mais a direita que funciona como um map.
 
-    c. concat [[a,'-'] | a <­ "paralelepipedo", a `elem` "aeiou"]
+    c. concat [[a, '-'] | a <- "paralelepipedo", a `elem` "aeiou"]
         pega as vogais da palavra paralelepipedo e concatena elas usando o -, isto é feito list
         comprehension, primeiro o comando a `elem` "aeiou" é executado sobre cada caracter da 
         lista de caracteres (string) paralelepipedo, tomando a ação de um filter por se localizar
@@ -81,10 +81,3 @@ _numera i (x:xs) = (i, x) : _numera (i + 1) xs
         map montando uma lista de caracteres (string) concatenando cada caracter da lista resultante
         e por último o concat junta estas strings que se encontram em uma lista de string.
 -}
-
-
-
-
-
-
-[ (x,y) | x <­ [1..5], even x, y <­ [(x + 1)..6], odd y ]
