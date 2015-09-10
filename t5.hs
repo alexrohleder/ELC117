c5b08ca02e0266898c939bc1fb24a50727b65fcc
Alex Rohleder <alexrohleder96@outlook.com>
2015-09-09 23:04:33 -0300

    9 and 11 added

--- t5.hs
+++ t5.hs
@@ -55,8 +55,8 @@ combine (x:xs) (y:ys) = [(x, y)] ++ combine xs ys
     de palavras e retorne outra lista contendo tuplas com as palavras numeradas
     a partir de 1. 
 -}
-numera :: [String] -> [(Int,String)]
-numera = _numera 1
+numera :: [String] -> [(Int, String)]
+numera s = _numera 1 s
 
 _numera :: Int -> [String] -> [(Int, String)]
 _numera _ [] = []
@@ -80,4 +80,44 @@ _numera i (x:xs) = (i, x) : _numera (i + 1) xs
         no lado direito da list comprehension, logo após o lado direito executa algo como um list
         map montando uma lista de caracteres (string) concatenando cada caracter da lista resultante
         e por último o concat junta estas strings que se encontram em uma lista de string.
+-}
+
+{-
+    (G. Malcolm, Univ. Liverpool) Write a function crossProduct :: [a] ­> [b] ­> [(a,b)] that takes two lists xs and ys, 
+    and returns the list of all possible pairings: [ (x,y) | x <­ xs, y <­ ys ] without using the above list comprehension. 
+    (As an exercise in problem decomposition, try first defining a "helper" function pairWithAll :: a ­> [b] ­> [(a,b)] 
+        that pairs its first argument with each element in its second.)
+-}
+crossProduct :: [a] -> [b] -> [(a, b)]
+crossProduct _ [] = []
+crossProduct [] _ = []
+crossProduct (x:xs) ys = pairWithAll x ys ++ crossProduct xs ys
+
+pairWithAll :: a -> [b] -> [(a, b)]
+pairWithAll _ [] = []
+pairWithAll x (y:ys) = (x, y) : pairWithAll x ys
+
+{-
+    Suponha que um retângulo seja representado por uma tupla (Float,Float,Float,Float), contendo respectivamente as
+    coordenadas x e y do ponto no seu canto superior esquerdo, seguidas das suas medidas de largura e altura. 
+    Sabendo que o eixo x cresce de cima para baixo e o eixo y da esquerda para direita, crie uma função 
+    genRects :: Int ­> (Int,Int) ­> [(Float,Float,Float,Float)] que receba um número N e um ponto (x,y) e gere uma sequência 
+    de N retângulos não sobrepostos. Os retângulos devem ser alinhados pelos seus topos, a partir do ponto dado, 
+    com largura e altura constantes. 
+-}
+
+{-
+    Escreva uma função recursiva que receba uma lista de tuplas e decomponha cada uma delas, gerando uma tupla de listas.
+-}
+listToTupla :: [(a, b)] -> ([a], [b])
+listToTupla [] = ([], [])
+listToTupla ((x,y):z) = ( x:(fst(listToTupla z)) , y:(snd(listToTupla z)) )
+
+{-
+    Refaça o exercício anterior usando list comprehension.
+
+listToTuplaLC :: [(a, b)] -> ([a], [b])
+listToTuplaLC [] = ([], [])
+listToTuplaLC list = ([], [])
+
 -}
