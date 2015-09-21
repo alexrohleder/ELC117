3bbdf8b7747d74230e02cf7d3d5e6de5775b9ae2
Administrator <admin@example.com>
2015-09-21 20:58:10 -0300

    p2 com erro na coloração

--- t6/t6-p2.hs
+++ t6/t6-p2.hs
@@ -1,13 +1,13 @@
 import Text.Printf
 
 type Point = (Float, Float)
-type Rect  = (Point, Float, Float)
+type Rect  = (Float, Point, Float, Float)
 
 row_num = 8
 col_num = 5
 
-getHslColor :: Int -> Int -> String
-getHslColor col_itr row_itr =
+getHslColor :: Float -> Int -> Int -> String
+getHslColor color col_itr row_itr =
     let
 
         col = realToFrac col_itr
@@ -18,25 +18,25 @@ getHslColor col_itr row_itr =
         rows = realToFrac row_num
         l = ((rows + 1) - row) / rows * 100
 
-    in  "hsl(360, " ++ (show (120 - s)) ++ "%, " ++ (show l) ++ "%)"
+    in  "hsl(" ++ show(floor color) ++ ", " ++ (show (120 - s)) ++ "%, " ++ (show l) ++ "%)"
 
 makeRect :: Float -> Float -> Float -> Float -> String -> String
-makeRect x y w h f = printf "<rect x='%.3f' y='%.3f' width='%.2f' height='%.2f' fill='%s' stroke='gray' stroke-width='2'/>" x y w h f
+makeRect x y w h f = printf "<rect x='%.3f' y='%.3f' width='%.2f' height='%.2f' fill='%s' stroke='gray' stroke-width='2'/>\n" x y w h f
 
 writeRect :: [Rect] -> Int -> Int -> String 
-writeRect [] _ _ _ = ""
-writeRect (((x, y), w, h):rs) 0 m x =
+writeRect [] _ _ = ""
+writeRect ((c, (x, y), w, h):rs) 0 m =
     let
-        f = getHslColor 0 m
+        f = getHslColor c 0 m
     in (makeRect x y w h f) ++ (writeRect rs col_num (m + 1))
-writeRect (((x, y), w, h):rs) n m x =
+writeRect ((c, (x, y), w, h):rs) n m =
     let
-        f = getHslColor n m
+        f = getHslColor c n m
     in (makeRect x y w h f) ++ (writeRect rs (n - 1) m)
 
 writeRects :: Float -> Float -> [Rect] -> String
 writeRects w h r =
-    printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>%s</svg>" w h (writeRect r col_num 1 0)
+    printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>\n%s</svg>" w h (writeRect r col_num 1)
 
 main :: IO ()
 main =
@@ -44,13 +44,16 @@ main =
 
         rect_width  = 44
         rect_height = 20
+        palette_margin = 10
+        palette_count = 3
 
-        xs = [0, rect_width..rect_width * 5]
+        xs = concat [[x, x + rect_width .. x + rect_width * 5] | x <- [0, rect_width * 6 + palette_margin .. (rect_width * 6) * palette_count]]
         ys = [0, rect_height..rect_height * 8]
+        cs = [360, 360 - (360 / palette_count) .. (360 / palette_count)]
 
         pw = last xs + rect_width
         ph = last ys + rect_height
 
-        rects = [((x, y), rect_width, rect_height) | y <- ys, x <- xs]
+        rects = [(c, (x, y), rect_width, rect_height) | c <- cs, y <- ys, x <- xs]
      
     in writeFile "colors.svg" $ writeRects pw ph rects
