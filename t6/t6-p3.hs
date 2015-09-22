91da2b4f5e794407823bad281f19e6038ff8beb5
Administrator <admin@example.com>
2015-09-21 22:43:44 -0300

    p3 main1 finalizada

--- t6/t6-p3.hs
+++ t6/t6-p3.hs
@@ -1,41 +1,25 @@
+
 import Text.Printf
 
-type Point = (Float, Float)
-type Rect  = (Float, Point, Float, Float)
+type Point = (Double, Double)
+type Rect  = (Double, Point, Double, Double)
 
-row_num = 8
-col_num = 5
-
-getHslColor :: Float -> String
+getHslColor :: Double -> String
 getHslColor color = "hsl(" ++ show(floor color) ++ ", 100%, 50%)"
 
-makeRect :: Float -> Float -> Float -> Float -> String -> String
+makeRect :: Double -> Double -> Double -> Double -> String -> String
 makeRect x y w h f = printf "<rect x='%.3f' y='%.3f' width='%.2f' height='%.2f' fill='%s' />" x y w h f
 
 writeRect :: [Rect] -> String 
 writeRect [] = ""
 writeRect ((c, (x, y), w, h):rs) =
-    let
-        f = getHslColor c
+    let f = getHslColor c
     in (makeRect x y w h f) ++ (writeRect rs)
 
-writeRects :: Float -> Float -> [Rect] -> String
-writeRects w h r =
-    printf "<svg width='%.2f' height='%.2f' xmlns='http://www.w3.org/2000/svg'>%s</svg>" w h (writeRect r)
+writeRects :: [Rect] -> String
+writeRects r = printf "<svg xmlns='http://www.w3.org/2000/svg'>%s</svg>" (writeRect r)
 
 main1 :: IO ()
 main1 =
-    let
-
-        rect_width  = 2
-        rect_height = 10
-
-        xs = [0, rect_width..rect_width * 360]
-        ys = [0]
-
-        pw = last xs + rect_width
-        ph = last ys + rect_height
-
-        rects = [(x, (x, y), rect_width, rect_height) | y <- ys, x <- xs]
-     
-    in writeFile "colors3.svg" $ writeRects pw ph rects
+    let rects = [(x, (x, y), 2.0, 10.0) | y <- [0.0], x <- [0.0, 2.0 .. 2.0 * 360]]
+    in writeFile "colors3.svg" $ writeRects rects
